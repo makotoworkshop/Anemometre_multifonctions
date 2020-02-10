8e9ea901a1b52f70e8408f4d6c49c923a79b1bd0
makotoworkshop <makoto@debianbox.makotoworkshop.org>
2020-02-10 17:45:10 +0100

    Delete config.h

--- Client_Anemometre_Monitoring_esp8266/config.h
+++
@@ -1,9 +0,0 @@
-//———— Librairies ————
-
-const char char_LF = 10;
-const char char_SPACE = 32; // charactère ESPACE ascii
-const char char_VT = 124; // vertical tab (VT)
-
-//———— Definition I/O LCD ————//
-#define txPin               D5
-#define rxPin               D6
